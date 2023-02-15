5417f5d491061fae93de90ee58d9af1e435e0314
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-15 02:54:54 -0500

    Make use of jquery to populate document based on ajax send and recieve

--- public/scripts/story_create_form.js
+++ public/scripts/story_create_form.js
@@ -7,57 +7,64 @@
 const generateRandomString = function () {
   let result = [];
   let charas = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  for (let i = 0 ; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     let rand = Math.floor(Math.random() * (charas.length - 1) + 1);
     result.push(charas[rand]);
   }
   return result.join('');
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
   const area = document.getElementsByName("story")[0];
   const counter = document.getElementsByName("counter")[0];
   const show = document.getElementById("show");
   prevwork = document.getElementById("oldentry");
+  const oldwork = document.location.href.split("/")
   let number = 140
 
-  $('#oldentry').text("previous entry here");
- 
-  $("#writingform").on('submit',function(event) {
+  //if the story is continuation
+
+  //checks to see if the url 
+  if (oldwork.length > 4) {
+    $.get("/api/stories")
+      .then(res => {
+        $('#oldentry').text(res.body);
+      }).catch(err =>{})
+  } else { //probably need to remove this else.
+    $('#oldentry').remove();
+  }
+
+
+  $("#writingform").on('submit', function (event) {
     event.preventDefault();
     console.log("abc");
     const data = $(this).serialize();
     console.log(data);
-    //addStory()
-    $.post("/api/stories",data)
-      .then(res =>{
+
+    //if the url contains id, then submit as contribution
+    if (oldwork.length > 4) {
+      $.post(`/api/stories/${oldwork[4]}`, data)
+      .then(res => {
         console.log(res);
       })
-
-
-
-    // $.ajax({
-    //   url:`/create`,
-    //   method:"POST",
-    //   data : $(this).serialize(),
-    //   datatype: JSON,
-    //   success : (ev) => {
-    //    console.log($(this).serialize());
-    //    $writing.value = "";
-    //   }
-    // }); 
+    } else {
+      $.post("/api/stories", data)
+      .then(res => {
+        console.log(res);
+      })
+    }
 
   })
 
 
-  $(show).on('click', function(event) {
+  $(show).on('click', function (event) {
     event.preventDefault();
     console.log("shoooowwww");
     $('#oldentry').slideToggle("slow");
   });
 
 
-  $(area).on("input paste focus",function() {
+  $(area).on("input paste focus", function () {
     number = area.textLength
     counter.textContent = number;
   })
